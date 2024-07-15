decd8f64d580f70525e818d26fee621b9edbf84b
Trav <travis.vas@gmail.com>
2024-07-15 12:45:41 -0400

    Confirmed test using API to classify a vendor by name.

--- working2.py
+++
@@ -1,8 +0,0 @@
-import os
-from openai import OpenAI
-
-api_key = os.environ.get("OPENAI_API_KEY")
-if not api_key:
-    raise ValueError("OPENAI_API_KEY environment variable is not set.")
-client = OpenAI(api_key=api_key)
-print("OpenAI API key is set.")
